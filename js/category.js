e7eab57eea13427d3ce85d15c4b38dc9759cf531
YuliaBondar <uliabondar359@gmail.com>
2024-09-06 01:34:07 +0300

    add model win

--- js/category.js
+++ js/category.js
@@ -7,35 +7,35 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     const floorMirror = [
-        { id: 1, photo: '../images/floorMirror/sec.png', name: "Kristin", text: 'Зеркало напольное', price: '150 000' },
-        { id: 2, photo: '../images/floorMirror/third.png', name: "Arlene", text: 'Зеркало напольное', price: '150 000' },
-        { id: 3, photo: '../images/floorMirror/fourth.png', name: "Colleen", text: 'Зеркало напольное', price: '150 000' },
-        { id: 4, photo: '../images/floorMirror/fifth.png', name: "Coppelia", text: 'Зеркало напольное', price: '150 000' },
-        { id: 5, photo: '../images/floorMirror/six.png', name: "Artemide", text: 'Зеркало напольное', price: '150 000' }
+        { id: 1, photo: '../images/floorMirror/sec.png', name: "Kristin", text: 'Напольное зеркало',color:'#A5D4FF', price: '150 000' },
+        { id: 2, photo: '../images/floorMirror/third.png', name: "Arlene", text: 'Напольное зеркало',color:'#F0B3EA', price: '150 000' },
+        { id: 3, photo: '../images/floorMirror/fourth.png', name: "Colleen", text: 'Напольное зеркало',color:'#8ECDFA', price: '150 000' },
+        { id: 4, photo: '../images/floorMirror/fifth.png', name: "coppelia", text: 'Напольное зеркало',color:'#FBE4CA', price: '150 000' },
+        { id: 5, photo: '../images/floorMirror/six.png', name: "artemide", text: 'Напольное зеркало',color:'#F0B5FA', price: '150 000' }
     ];
 
     const floorLamp = [
-        { id: 1, photo: '../images/floorLamp/lamp1.png', name: "Aubrey", text: 'Лампа настольная', price: '150 000' },
-        { id: 2, photo: '../images/floorLamp/lamp2.png', name: "Leslie", text: 'Лампа настольная', price: '150 000' },
-        { id: 3, photo: '../images/floorLamp/lamp3.png', name: "Darrell", text: 'Лампа настольная', price: '150 000' },
-        { id: 4, photo: '../images/floorLamp/lamp4.png', name: "Coppelia", text: 'Лампа настольная', price: '150 000' },
-        { id: 5, photo: '../images/floorLamp/lamp5.png', name: "Artemide", text: 'Лампа настольная', price: '150 000' }
+        { id: 1, photo: '../images/floorLamp/lamp1.png', name: "Aubrey", text: 'Лампа настольная',color:'#F0B5FA', price: '150 000' },
+        { id: 2, photo: '../images/floorLamp/lamp2.png', name: "Leslie", text: 'Лампа настольная',color:'#88E379', price: '`150 000' },
+        { id: 3, photo: '../images/floorLamp/lamp3.png', name: "Darrell", text: 'Лампа настольная',color:'#FA8EEF', price: '150 000' },
+        { id: 4, photo: '../images/floorLamp/lamp4.png', name: "Coppelia", text: 'Лампа настольная',color:'#29D0D9', price: '150 000' },
+        { id: 5, photo: '../images/floorLamp/lamp5.png', name: "Artemide", text: 'Лампа настольная',color:'##89D5FF', price: '150 000' }
     ];
 
     const armchairsChairs = [
-        { id: 1, photo: '../images/armchairsСhairs/ch1.png', name: "Kathryn", text: 'Кресло', price: '150 000' },
-        { id: 2, photo: '../images/armchairsСhairs/ch2.png', name: "Wendy", text: 'Кресло', price: '150 000' },
-        { id: 3, photo: '../images/armchairsСhairs/ch3.png', name: "Judith", text: 'Кресло', price: '150 000' },
-        { id: 4, photo: '../images/armchairsСhairs/ch4.png', name: "Jane", text: 'Кресло', price: '150 000' },
-        { id: 5, photo: '../images/armchairsСhairs/ch5.png', name: "Darlene", text: 'Кресло', price: '150 000' }
+        { id: 1, photo: '../images/armchairsСhairs/ch1.png', name: "Kathryn", text: 'Зеркало напольное',color:'#FBE4CA', price: '150 000' },
+        { id: 2, photo: '../images/armchairsСhairs/ch2.png', name: "Wendy", text: 'Зеркало напольное',color:'#F0B3EA', price: '150 000' },
+        { id: 2, photo: '../images/armchairsСhairs/ch3.png', name: "Judith", text: 'Зеркало напольное',color:'#F18EFA', price: '150 000' },
+        { id: 2, photo: '../images/armchairsСhairs/ch4.png', name: "Jane", text: 'Зеркало напольное',color:'#5C544A', price: '150 000' },
+        { id: 2, photo: '../images/armchairsСhairs/ch5.png', name: "Darlene", text: 'Зеркало напольное',color:'#F0B5FA', price: '150 000' }
     ];
 
     const tablesCabinets = [
-        { id: 1, photo: '../images/tablesСabinets/tabel1.png', name: "Kathryn", text: 'Стол', price: '150 000' },
-        { id: 2, photo: '../images/tablesСabinets/table2.png', name: "Wendy", text: 'Стол', price: '150 000' },
-        { id: 3, photo: '../images/tablesСabinets/table3.png', name: "Judith", text: 'Стол', price: '150 000' },
-        { id: 4, photo: '../images/tablesСabinets/table4.png', name: "Jane", text: 'Стол', price: '150 000' },
-        { id: 5, photo: '../images/tablesСabinets/table5.png', name: "Darlene", text: 'Стол', price: '150 000' }
+        { id: 1, photo: '../images/tablesСabinets/tabel1.png', name: "Kathryn", text: 'Зеркало напольное',color:'#A5D4FF', price: '150 000' },
+        { id: 2, photo: '../images/tablesСabinets/table2.png', name: "Wendy", text: 'Зеркало напольное',color:'#F0DFB3', price: '150 000' },
+        { id: 3, photo: '../images/tablesСabinets/table3.png', name: "Judith", text: 'Зеркало напольное',color:'#F88EFA', price: '150 000' },
+        { id: 4, photo: '../images/tablesСabinets/table4.png', name: "Jane", text: 'Зеркало напольное',color:'#CACFFB', price: '150 000' },
+        { id: 5, photo: '../images/tablesСabinets/table5.png', name: "Darlene", text: 'Зеркало напольное',color:'#F0B5FA', price: '150 000' }
     ];
 
     const contentCategory = document.getElementById('content-category');
@@ -51,13 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 <div class="for-col" style="background-color: pink; border-radius: 40%;">${quantity}</div>
             </div>
             <div class="arrow">
-                <button onclick="showDetails(${id})"><i class="fa fa-arrow-down"></i></button>
+                <button onclick="toggleDetails(${id})"><i class="fa fa-arrow-down"></i></button>
             </div>
         `;
         contentCategory.appendChild(categoryDiv);
     });
 
-    function showDetails(id) {
+    window.toggleDetails = function(id) {
+        if (contentDetails.innerHTML !== '') {
+            contentDetails.innerHTML = '';
+            return;
+        }
+
         let items;
         switch (id) {
             case 1:
@@ -77,16 +82,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         contentDetails.innerHTML = ''; 
-        items.forEach(({ photo, name, text, price }) => {
-            const detailDiv = document.createElement('div');
-            detailDiv.classList.add('detail');
-            detailDiv.innerHTML = `
-                <img style="width:100px;" src="${photo}">
-                <p>${name}</p>
-                <p>${text}</p>
-                <p>${price}</p>
-            `;
-            contentDetails.appendChild(detailDiv);
-        });
+
+// Добавляем элемент с количеством позиций один раз
+// const categoryDiv = document.createElement('div');
+// categoryDiv.innerHTML = '20 позиций в категории';
+// contentDetails.appendChild(categoryDiv);
+
+items.forEach(({ photo, name, text, price, color }) => {
+    const detailDiv = document.createElement('div');
+    detailDiv.classList.add('detail');
+    detailDiv.innerHTML = `
+        <div style="background-color:${color}; width:10px; height:10px; border-radius:50%; border: 2px solid white;"></div>
+        <img style="width:100px;" src="${photo}">
+        <p>${name}</p>
+        <p>${text}</p>
+        <hr class="yellow">
+        <p >${price} <sup>₽</sup></p>
+    `;
+    contentDetails.appendChild(detailDiv);
+});
+
     }
 });
---
+++ js/modalContact.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modalContact');
+const modalFormContact = document.getElementById('modalFormContact');
+
+modalFormContact.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
+function closeModal() {
+    document.getElementById("modalContact").style.display = "none";
+    // document.getElementById("modalContact").style.display = "block";
+}
